ea7de5e94a13526caf57dcd635f390fc21a56f33
Dan Butts <dab@umd.edu>
2020-01-31 17:51:17 -0500

    more modest update to ConvLayer
    ceil vs floor with strides/dilation

--- layer.py
+++ layer.py
@@ -482,9 +482,9 @@ class ConvLayer(Layer):
         # Calculate number of shifts (for output)
         num_shifts = [1, 1]
         if input_dims[1] > 1:
-            num_shifts[0] = int(np.floor(input_dims[1]/shift_spacing))
+            num_shifts[0] = int(np.ceil(input_dims[1]/shift_spacing))
         if input_dims[2] > 1:
-            num_shifts[1] = int(np.floor(input_dims[2]/shift_spacing))
+            num_shifts[1] = int(np.ceil(input_dims[2]/shift_spacing))
 
         super(ConvLayer, self).__init__(
                 scope=scope,
